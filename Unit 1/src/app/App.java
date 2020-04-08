fd39f9d47f2361d2cf41e8765c1c0a94f35ea9d5
angeladanger <819022@mystma.org>
2020-04-08 15:01:18 +0000

    Added top five movies

--- Unit 1/src/app/App.java
+++ Unit 1/src/app/App.java
@@ -2,6 +2,10 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println("1. Your Name");
+        System.out.println("2. A Silent Voice");
+        System.out.println("3. Hotarubi no Mori E");
+        System.out.println("4. The Garden of Words");
+        System.out.println("5. Spirited Away");
     }
 }
