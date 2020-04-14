1e6a344e5a8c3d7916c2c7088e0cb62d64d27d73
angeladanger <819022@mystma.org>
2020-04-14 15:31:45 +0000

    Added While Loops

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,20 +2,30 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String person1 = "Tim";
-        String person2 = "Bob";
-        String verb1 = "run";
-        String verb2 = "walk";
-        String verb3 = "crawl";
-        String food = "eggrolls";
-        String dayOfWeek = "Friday";
-        String place1 = "Tim's house";
-        String place2 = "Bob's house";
-        String adjective1 = "blue";
-        String adjective2 = "round";
+        int count = 0;
+        String color = "yellow";
+        boolean answer = true
 
-        System.out.println(person1 + "is a programmer who likes to" + verb1);
-        System.out.println("Every" + dayOfWeek + "they make a" + adjective1 food + "for breakfast and" + verb2 + "at" + place1 + "with" + person2);
-        System.out.println("Sometimes, they go to the" + adjective2 + place2 + "with" + person2 + "so that they can" + verb3 + "together.");
-    }
+
+        while (count <= 15) {
+            System.out.println("The count is " + count);
+            count = count + 5;
+        }
+
+        System.out.println("count finished");
+
+        while (color == "yellow"); {
+            System.out.println("The color is" + color);
+            color = "pink";
+        }
+
+        System.out.println("color finished");
+
+        while (answer == true); {
+            System.out.println("Do we live in Minnesota?" + answer);
+            answer = false;
+        }
+
+        System.out.println("answer finished");
+     }
 }
