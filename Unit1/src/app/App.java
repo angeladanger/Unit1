ce6701674b6b5a430634e28d290563c4e1dd01aa
angeladanger <819022@mystma.org>
2020-04-16 16:57:49 +0000

    Added For Loops Challenge

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,25 +2,25 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        for (int i = 10; i <= 20; i = i+1) {
-            System.out.println("The count is" +i);
-            System.out.println("Let's keep counting.");
+        for (int i = 22; i <= 28; i = i+3) {
+            System.out.println(+i);
         }
-        for (int i = 10; i <= 100; i = i+10) {
-            System.out.println("The count is" +i);
-            System.out.println("Let's keep counting.");
+        for (int m = 2; m <= 32; m = m*2) {
+            System.out.println(+m);
         }
-        for (int i = 1000; i <= 256000; i = i+1000) {
-            System.out.println("The count is" +i);
-            System.out.println("Let's keep counting.");
+        for (int i = 3; i >= -81; i = i * -i) {
+            System.out.println(+i);
         }
-        for (int i = 11; i <= 12; i = i+1) {
-            System.out.println("The count is" +i);
-            System.out.println("Let's keep counting.");
+        for (int i = 50; i >= 10; i = i - 5) {
+            System.out.println(+i);
         }
-        for (int i = 10; i >= 0; i = i-1) {
-            System.out.println("The count is" +i);
-            System.out.println("Let's keep counting.");
+        for (int i = 1; i <= 12; i = i+1) {
+            System.out.println(+i);
         }
+        for (int i = 2; i <= 81; i = i^(2)) {
+            System.out.println(+i);
+        }
+
+
     }
 }
