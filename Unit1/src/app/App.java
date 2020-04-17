4d968210731e9f0559f7a5ca558c29c3f96727d9
angeladanger <819022@mystma.org>
2020-04-17 15:01:20 +0000

    Added For Loops Challenge Updated

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,22 +3,21 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
         for (int i = 22; i <= 28; i = i+3) {
-            System.out.println(+i);
+            System.out.println(i);
         }
         for (int m = 2; m <= 32; m = m*2) {
-            System.out.println(+m);
+            System.out.println(m);
         }
         for (int i = 3; i >= -81; i = i * -i) {
-            System.out.println(+i);
+            System.out.println(i);
         }
-        for (int i = 50; i >= 10; i = i - 5) {
-            System.out.println(+i);
+        for (int m = 50; m >= 5; m = m - 5) {
+            System.out.println(m);
         }
-        for (int i = 1; i <= 12; i = i+1) {
-            System.out.println(+i);
-        }
-        for (int i = 2; i <= 81; i = i^(2)) {
-            System.out.println(+i);
+        for (int i = 1; i <= 9; i = i+1) {
+            System.out.println(i * i);
+            System.out.println(i+3);
+            System.out.println(i);
         }
 
 
