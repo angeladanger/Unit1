095aedcd0acbf6c01df9b90d971b27aad596ead3
angeladanger <819022@mystma.org>
2020-04-20 15:45:16 +0000

    Added If Statements

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,24 +2,15 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        for (int i = 22; i <= 28; i = i+3) {
-            System.out.println(i);
-        }
-        for (int m = 2; m <= 32; m = m*2) {
-            System.out.println(m);
-        }
-        for (int i = 3; i >= -81; i = i * -i) {
-            System.out.println(i);
-        }
-        for (int m = 50; m >= 5; m = m - 5) {
-            System.out.println(m);
-        }
-        for (int i = 1; i <= 9; i = i+1) {
-            System.out.println(i * i);
-            System.out.println(i+3);
-            System.out.println(i);
+
+        for (int i = 0; i <= 20; i++) {
+            if (i % 4 == 0) {
+                System.out.println(i + " is divisible by 4");
+            } else {
+                System.out.println(i + " is not divisible by 4");
+            }
         }
 
-
+        System.out.println("All finished");
     }
 }
