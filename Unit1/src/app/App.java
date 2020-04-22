cf68cba9aa00ac568ffab5f9b514b97a5b6ce5d0
angeladanger <819022@mystma.org>
2020-04-22 15:09:25 +0000

    Added Loops Challenge #2

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,25 +3,23 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        System.out.println("starting loop 1...");
+        System.out.println("Start first loop");
 
-        for (int i = 0; i <= 19; i++) {
-            if (i % 1 == 0) {
-                System.out.println(i);
+        for (int i = 20; i >= 0; i = i - 1){
+            System.out.println(i);
+            if (i == 10){
+                for (int j = 0; j < 7; j++){
+                    System.out.println(i);
+                }
             }
         }
-
-        System.out.println("end of loop 1.");
-
-        System.out.println("starting loop 2...");
-
-        for (int i = -1; i <= 19; i = i + 3) {
-            if (i % 1 == 0) {
-                System.out.println(i + 3);
-                System.out.println(i + 3);
-            }
+        System.out.println("End first loop");
+        System.out.println("Start second loop");
+        for (int i = 4; i <= 36; i = i + 4){
+            if (i % 4 == 0)
+            
+            System.out.println("Hey!");
+            System.out.println(i);
         }
-
-        System.out.println("end of loop 2.");
     }
 }
