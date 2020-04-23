2dcac53eb307168b682ca147bf00bbdb565331a7
angeladanger <819022@mystma.org>
2020-04-23 14:45:29 +0000

    Added Loops Challenge #3

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,24 +3,27 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Start first loop");
+        System.out.println("Double or single digit?");
 
-        for (int i = 20; i >= 1; i = i - 1){
-            System.out.println(i);
-            if (i == 10){
-                for (int j = 0; j < 7; j++){
-                    System.out.println(i);
-                }
+        for (int i = 20; i >= 1; i = i - 1) {
+            if (i > 9){
+                System.out.println(i + " is a double digit");
+            }
+            else {
+                System.out.println(i + " is a single digit");
             }
         }
-        System.out.println("End first loop");
-        System.out.println("Start second loop");
-        for (int i = 0; i <= 36; i = i + 4){
-            if (i % 6 == 0)
+        System.out.println("Nice!");
+        System.out.println("Fast Food");
 
-            System.out.println("Hey!");
-            System.out.println(i);
+        for (int i = 1; i <= 10; i = i + 1) {
+            if (i % 2 == 0) {
+                System.out.println("Fries");
+            }
+            else {
+                System.out.println("Burgers");
+            }
         }
-        System.out.println("End of second loop");
+        System.out.println("Nice!");
     }
 }
