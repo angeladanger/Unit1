07816dd50605bcf8c22ac50181956f073f5b82ce
angeladanger <819022@mystma.org>
2020-04-30 12:57:48 +0000

    Added Scanner Challenge Updated

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -5,12 +5,10 @@ public class App {
     public static void main(final String[] args) throws Exception {
      Scanner input = new Scanner(System.in);
 
-     int myInt = 183726073;
-
         System.out.println("Please enter the password.");
-        String password = input.nextLine();
+        String enteredPassword = input.nextLine();
 
-        if (myInt == 183726073) {
+        if (enteredPassword.equals("183726073")){
             System.out.println("ACCESS GRANTED");
     }
         else{
