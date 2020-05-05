fe53a356afda40735cdce2843545c7f4ac9b3e66
angeladanger <819022@mystma.org>
2020-05-05 15:16:11 +0000

    Added Three Question Quiz

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -5,13 +5,35 @@ public class App {
     public static void main(final String[] args) throws Exception {
      Scanner input = new Scanner(System.in);
 
-     System.out.println("What is the count?");
-     int count = input.nextInt();
-     
-     for (int i = 1; i <= count; i = i + 1){
-         System.out.println(i);
+     System.out.println("How many countries are in the world?");
+     String countriesInWorld = input.nextLine();
+
+     if (countriesInWorld.equals("195")){
+         System.out.println("CORRECT");
      }
-     
+     else{
+         System.out.println("INCORRECT");
+     }
+
+     System.out.println("What is the capital of America?");
+     String capitalAmerica = input.nextLine();
+
+     if (countriesInWorld.equals("Washington DC")){
+         System.out.println("CORRECT");
+     }
+     else{
+         System.out.println("INCORRECT");
+     }
+
+     System.out.println("How old is the continent of North America?");
+     String ageOfAmerica = input.nextLine();
+
+     if (ageOfAmerica.equals("200 million years old")){
+         System.out.println("CORRECT");
+     }
+     else{
+         System.out.println("INCORRECT");
+     }
 
 
     }
