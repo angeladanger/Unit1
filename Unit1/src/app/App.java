dc40645aeaa5e9cab8a75f6b9181621c61fd38db
angeladanger <819022@mystma.org>
2020-05-06 16:04:32 +0000

    Added Madlibs with user output

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -5,35 +5,35 @@ public class App {
     public static void main(final String[] args) throws Exception {
      Scanner input = new Scanner(System.in);
 
-     System.out.println("How many countries are in the world?");
-     String countriesInWorld = input.nextLine();
+    System.out.println("PLEASE ENTER A NOUN");
+    String noun1 = input.nextLine();
+    System.out.println("PLEASE ENTER A VERB");
+    String verb1 = input.nextLine();
+    System.out.println("PLEASE ENTER AN ADJECTIVE");
+    String adjective1 = input.nextLine();
+    System.out.println("PLEASE ENTER AN ADJECTIVE AND A NOUN");
+    String adjectiveAndNoun = input.nextLine();
+    System.out.println("PLEASE INSERT A NUMBER");
+    String number1 = input.nextLine();
+    System.out.println("PLEASE ENTER A FOOD");
+    String food1 = input.nextLine();
+    System.out.println("PLEASE INSERT A TIME");
+    String time1 = input.nextLine();
+    System.out.println("PLEASE INSERT A SECOND ADJECTIVE");
+    String adjective2 = input.nextLine();
+    System.out.println("PLEASE ENTER ANOTHER VERB");
+    String verb2 = input.nextLine();
+    System.out.println("PLEASE ENTER A LOCATION");
+    String location1 = input.nextLine();
 
-     if (countriesInWorld.equals("195")){
-         System.out.println("CORRECT");
-     }
-     else{
-         System.out.println("INCORRECT");
-     }
+    System.out.println("Yesterday, " + noun1 + " and I " + verb1 + " to the " + adjective1 + " restaurant. On our way there, we saw a " + adjectiveAndNoun + " which caught our attention.");
+    System.out.println("We proceeded into the restaurant. As we ordered, there were about " + number1 + " items on the menu. I was very excited to see that " + food1 + " was on the menu.");
+    System.out.println("It was around " + time1 + " when we decided to go home. The food I ate today was " + adjective2 + " but very good nonetheless.");
+    System.out.println("When I arrived at home, I " + verb2 + " into my " + location1 + " and knocked out. The end!");
 
-     System.out.println("What is the capital of America?");
-     String capitalAmerica = input.nextLine();
 
-     if (countriesInWorld.equals("Washington DC")){
-         System.out.println("CORRECT");
-     }
-     else{
-         System.out.println("INCORRECT");
-     }
 
-     System.out.println("How old is the continent of North America?");
-     String ageOfAmerica = input.nextLine();
 
-     if (ageOfAmerica.equals("200 million years old")){
-         System.out.println("CORRECT");
-     }
-     else{
-         System.out.println("INCORRECT");
-     }
 
 
     }
