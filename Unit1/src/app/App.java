df76ae1bedb22acd5aff1a0ead873b87eab4f9b8
angeladanger <819022@mystma.org>
2020-05-07 14:24:23 +0000

    Added Mad Lib with user outputs

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -5,31 +5,31 @@ public class App {
     public static void main(final String[] args) throws Exception {
      Scanner input = new Scanner(System.in);
 
-    System.out.println("PLEASE ENTER A NOUN");
+    System.out.println("PLEASE INSERT A NOUN");
     String noun1 = input.nextLine();
-    System.out.println("PLEASE ENTER A VERB");
+    System.out.println("PLEASE INSERT A VERB");
     String verb1 = input.nextLine();
-    System.out.println("PLEASE ENTER AN ADJECTIVE");
+    System.out.println("PLEASE INSERT AN ADJECTIVE");
     String adjective1 = input.nextLine();
-    System.out.println("PLEASE ENTER AN ADJECTIVE AND A NOUN");
+    System.out.println("PLEASE INSERT AN ADJECTIVE AND A NOUN");
     String adjectiveAndNoun = input.nextLine();
     System.out.println("PLEASE INSERT A NUMBER");
     String number1 = input.nextLine();
-    System.out.println("PLEASE ENTER A FOOD");
+    System.out.println("PLEASE INSERT A FOOD");
     String food1 = input.nextLine();
     System.out.println("PLEASE INSERT A TIME");
     String time1 = input.nextLine();
     System.out.println("PLEASE INSERT A SECOND ADJECTIVE");
     String adjective2 = input.nextLine();
-    System.out.println("PLEASE ENTER ANOTHER VERB");
+    System.out.println("PLEASE INSERT ANOTHER VERB");
     String verb2 = input.nextLine();
-    System.out.println("PLEASE ENTER A LOCATION");
+    System.out.println("PLEASE INSERT A LOCATION");
     String location1 = input.nextLine();
 
     System.out.println("Yesterday, " + noun1 + " and I " + verb1 + " to the " + adjective1 + " restaurant. On our way there, we saw a " + adjectiveAndNoun + " which caught our attention.");
     System.out.println("We proceeded into the restaurant. As we ordered, there were about " + number1 + " items on the menu. I was very excited to see that " + food1 + " was on the menu.");
     System.out.println("It was around " + time1 + " when we decided to go home. The food I ate today was " + adjective2 + " but very good nonetheless.");
-    System.out.println("When I arrived at home, I " + verb2 + " into my " + location1 + " and knocked out. The end!");
+    System.out.println("When I arrived at home, I " + verb2 + " into " + location1 + " and knocked out. The end!");
 
 
 
